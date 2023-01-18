994a2f026919dc93c54bdc701d27aabcaa6b5365
Mac_Developer <araozangel842@gmail.com>
2023-01-18 18:29:10 +0200

    Starting

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: const Text('Home Page', style: TextStyle(color: Colors.white)),
+      ),
+      body: Column(children: const [
+        Text(
+          'Theme data types',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+        ),
+      ]),
+    );
+  }
+}
