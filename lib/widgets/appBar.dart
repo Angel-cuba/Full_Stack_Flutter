6358c41871e4dbf541de55053ce5cc058e89b0c8
Mac_Developer <araozangel842@gmail.com>
2023-01-23 13:34:32 +0200

    Refactoring code

--- lib/widgets/appBar.dart
+++ lib/widgets/appBar.dart
@@ -27,42 +27,46 @@ class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeModel>(builder: (context, theme, child) {
-      return AppBar(
-        elevation: .1,
-        leading: GestureDetector(
-          onTap: () {},
-          child: const Icon(Icons.menu),
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {
-                theme.isDarkTheme
-                    ? theme.isDarkTheme = false
-                    : theme.isDarkTheme = true;
-                notifications.showNotification(
-                    id: 0,
-                    title: 'Theme changed',
-                    body: theme.isDarkTheme
-                        ? 'You have selected dark theme'
-                        : 'Nice choice, light theme');
-                //Schedule a notification
-                notifications.scheduleNotification(
-                    id: 0,
-                    title: 'Scheduled notification',
-                    body: 'This is a scheduled notification',
-                    seconds: 5);
-              },
-              icon: Icon(
-                  theme.isDarkTheme ? Icons.wb_sunny : Icons.nightlight_round),
-              color: theme.isDarkTheme
-                  ? Theme.of(context).iconTheme.color
-                  : Theme.of(context).iconTheme.color),
-          const SizedBox(width: 20),
-          const CircleAvatar(
-            // radius: 20,
-            backgroundImage: AssetImage('images/Angel_NON-bg.png'),
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: AppBar(
+          elevation: .1,
+          leading: GestureDetector(
+            onTap: () {},
+            child: const Icon(Icons.menu),
           ),
-        ],
+          actions: [
+            IconButton(
+                onPressed: () {
+                  theme.isDarkTheme
+                      ? theme.isDarkTheme = false
+                      : theme.isDarkTheme = true;
+                  notifications.showNotification(
+                      id: 0,
+                      title: 'Theme changed',
+                      body: theme.isDarkTheme
+                          ? 'You have selected dark theme'
+                          : 'Nice choice, light theme');
+                  //Schedule a notification
+                  notifications.scheduleNotification(
+                      id: 0,
+                      title: 'Scheduled notification',
+                      body: 'This is a scheduled notification',
+                      seconds: 5);
+                },
+                icon: Icon(theme.isDarkTheme
+                    ? Icons.wb_sunny
+                    : Icons.nightlight_round),
+                color: theme.isDarkTheme
+                    ? Theme.of(context).iconTheme.color
+                    : Theme.of(context).iconTheme.color),
+            const SizedBox(width: 10),
+            const CircleAvatar(
+              // radius: 20,
+              backgroundImage: AssetImage('images/Angel_NON-bg.png'),
+            ),
+          ],
+        ),
       );
     });
   }
