3fa9781bab5dc8e662df7fe21285d8f2b3d7b3c1
Mac_Developer <araozangel842@gmail.com>
2023-01-24 14:01:01 +0200

    Add select day from calendar function

--- lib/screens/add_task_bar.dart
+++ lib/screens/add_task_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:learning_flutter/ui/fonts/light_fonts.dart';
 import 'package:learning_flutter/ui/theme/theme_model.dart';
 import 'package:learning_flutter/widgets/appBar.dart';
@@ -15,6 +16,14 @@ class AddTaskBar extends StatefulWidget {
 }
 
 class _AddTaskBarState extends State<AddTaskBar> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  void initState() {
+    // selectedDate = DateTime.now();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeModel>(builder: ((context, theme, child) {
@@ -36,10 +45,64 @@ class _AddTaskBarState extends State<AddTaskBar> {
                       style: theme.isDarkTheme
                           ? darkHeadingStyle
                           : lightHeadingStyle),
-                  SizedBox(height: 20),
                   CustomInput(
                       title: 'Title',
                       hint: 'Enter your title',
+                      controller: controller),
+                  CustomInput(
+                      title: 'Description',
+                      hint: 'Enter your description',
+                      controller: controller),
+                  CustomInput(
+                      title: 'Date',
+                      hint: DateFormat.yMMMd().format(selectedDate),
+                      widget: IconButton(
+                          icon: const Icon(Icons.calendar_today),
+                          onPressed: () {
+                            void getDateFromUser() async {
+                              final DateTime? pickerDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(2020),
+                                  lastDate: DateTime(2025),
+                                  initialDatePickerMode: DatePickerMode.day,
+                                  builder:
+                                      (BuildContext context, Widget? child) {
+                                    return Theme(
+                                      data: ThemeData.light().copyWith(
+                                        colorScheme: ColorScheme.light(
+                                          primary: theme.isDarkTheme
+                                              ? Colors.black87
+                                              : Colors.teal,
+                                          onPrimary: Colors.white70,
+                                          surface: Colors.blue,
+                                          onSurface: Colors.black,
+                                        ),
+                                        dialogBackgroundColor: Colors.white,
+                                      ),
+                                      child: child!,
+                                    );
+                                  },
+                                  // Here we are disabling the weekends from the calendar
+                                  // Just saturday and sunday
+                                  selectableDayPredicate: (DateTime val) {
+                                    if (val.weekday == 6 || val.weekday == 7) {
+                                      return false;
+                                    }
+                                    return true;
+                                  });
+                              if (pickerDate != null) {
+                                setState(() {
+                                  selectedDate = pickerDate;
+                                });
+                                print(selectedDate);
+                              }
+                            }
+
+                            getDateFromUser();
+                          }
+                          // },
+                          ),
                       controller: controller),
                 ],
               ),
@@ -48,3 +111,16 @@ class _AddTaskBarState extends State<AddTaskBar> {
     }));
   }
 }
+
+// void _getDateFromUser(BuildContext context) async {
+//   final DateTime? pickerDate = await showDatePicker(
+//       context: context,
+//       initialDate: DateTime.now(),
+//       firstDate: DateTime(2020),
+//       lastDate: DateTime(2022));
+//   if (pickerDate != null && pickerDate != selectedDate) {
+//     setState(() {
+//       selectedDate = pickerDate;
+//     });
+//   }
+// }
