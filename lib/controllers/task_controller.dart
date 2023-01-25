5c44cb0d46ad83d7665a8304e5c6deba1fdc5151
Mac_Developer <araozangel842@gmail.com>
2023-01-25 03:34:17 +0200

    Add controller to send data to db

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import '../models/task.dart';
+
+class TaskController extends GetxController {
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  Future<void> addTask({Task? task}) async {}
+}
